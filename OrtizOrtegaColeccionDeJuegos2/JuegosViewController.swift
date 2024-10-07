e0aa70bbb2c2f5e07c69acf537d93afc51404873
MauroOrtiz1 <bernie.ortiz@tecsup.edu.pe>
2024-10-06 18:56:36 -0700

    Lab9 Tarea Acabada

--- OrtizOrtegaColeccionDeJuegos2/JuegosViewController.swift
+++ OrtizOrtegaColeccionDeJuegos2/JuegosViewController.swift
@@ -7,16 +7,49 @@
 
 import UIKit
 
-class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var tituloTextField: UITextField!
-    
+    @IBOutlet weak var agregarActualizarBoton: UIButton!
+    @IBOutlet weak var eliminarBoton: UIButton!
+    @IBOutlet weak var pickerView: UIPickerView!
+    let categorias = ["Acción", "Aventura", "Puzzle", "Deportes"]
+    var categoriaSeleccionada: String?
     var imagePicker = UIImagePickerController()
+    var juego:Juego? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        if let juego = juego {
+            imageView.image = UIImage(data: juego.imagen! as Data)
+            tituloTextField.text = juego.titulo
+            if let categoria = juego.categoria, let index = categorias.firstIndex(of: categoria) {
+                pickerView.selectRow(index, inComponent: 0, animated: false)
+            }
+            agregarActualizarBoton.setTitle("Actualizar", for: .normal)
+        } else {
+            eliminarBoton.isHidden = true
+        }
+    }
+    // UIPickerView DataSource
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categorias.count
+    }
+    // UIPickerView Delegate
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categorias[row]
+    }
+        
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        categoriaSeleccionada = categorias[row]
     }
     
     @IBAction func fotosTapped(_ sender: Any) {
@@ -26,19 +59,30 @@ class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, U
     @IBAction func camaraTapped(_ sender: Any) {
     }
     @IBAction func agregarTapped(_ sender: Any) {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        let juego = Juego(context: context)
-        juego.titulo = tituloTextField.text
-        juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        if juego != nil {
+            juego!.titulo! = tituloTextField.text!
+            juego!.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+            juego!.categoria = categoriaSeleccionada
+        } else {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let juego = Juego(context: context)
+            juego.titulo = tituloTextField.text
+            juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+            juego.categoria = categoriaSeleccionada
+        }
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
         navigationController?.popViewController(animated: true)
     }
-    
+    @IBAction func eliminarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        context.delete(juego!)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+    }
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let imagenSeleccionada = info[.originalImage] as? UIImage
         imageView.image = imagenSeleccionada
         imagePicker.dismiss(animated: true, completion: nil)
     }
-
 }
--- OrtizOrtegaColeccionDeJuegos2/ViewController.swift
+++ OrtizOrtegaColeccionDeJuegos2/ViewController.swift
@@ -15,11 +15,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let juego = juegos[indexPath.row]
-        cell.textLabel?.text = juego.titulo
+        
+        let titulo = juego.titulo ?? "Sin título"
+        let categoria = juego.categoria ?? "Sin categoría"
+        cell.textLabel?.text = "\(titulo) - \(categoria)"
         cell.imageView?.image = UIImage(data: (juego.imagen!))
         return cell
     }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let juegoToDelete = juegos[indexPath.row]
+            context.delete(juegoToDelete)
+            
+            do {
+                try context.save()
+                juegos.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            } catch {
+                print("Error al eliminar el juego")
+            }
+        }
+    }
+
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let juego = juegos[indexPath.row]
+        performSegue(withIdentifier: "juegoSegue", sender: juego)
+    }
     
     @IBOutlet weak var tableView: UITableView!
     var juegos : [Juego] = []
@@ -38,7 +62,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }catch{
         }
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let siguienteVC = segue.destination as! JuegosViewController
+        siguienteVC.juego = sender as? Juego
+    }
 
 }
 
